ee6245fb0ce6e6134100ffa590fe76fdeb4229ad
Bennett <bemobi7b@gmail.com>
2023-06-29 13:45:27 -0500

    WifiLab

---
+++ src/WiFi.cpp
@@ -0,0 +1,63 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/Rich/CTD2023/WiFi/src/WiFi.ino"
+void setup();
+void loop();
+#line 1 "c:/Users/Rich/CTD2023/WiFi/src/WiFi.ino"
+#define BLYNK_TEMPLATE_ID "TMPL258xouvzs"
+#define BLYNK_TEMPLATE_NAME "Tempy"
+#define BLYNK_AUTH_TOKEN "dIwqCXzJrXgrhRlXiXRvgIPqdl5UTBJx"
+
+#include <blynk.h>
+#include "oled-wing-adafruit.h"
+
+#define TEMPINPUT A4
+
+OledWingAdafruit display;
+uint64_t reading;
+
+SYSTEM_THREAD(ENABLED);
+void setup()
+{
+    Blynk.begin(BLYNK_AUTH_TOKEN);
+    display.setup();
+    display.clearDisplay();
+    display.display();
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.setCursor(0, 0);
+    pinMode(TEMPINPUT, INPUT);
+}
+void loop()
+{
+    Blynk.run();
+    display.loop();
+    reading = analogRead(TEMPINPUT);
+    double voltage = (reading * 3.3) / 4095.0;
+    double temperature = (voltage - 0.5) * 100;
+    int fahrenheit = temperature * 1.8 + 32;
+    int temp = temperature;
+    display.clearDisplay();
+    display.println("Current temperature: \n");
+    display.printf("%u C \n", temp);
+    display.printf("%u F", fahrenheit);
+    display.display();
+    if (display.pressedA())
+    {
+        display.clearDisplay();
+        display.println("Push Sent");
+        display.display();
+        delay(5000);
+    }
+}
+BLYNK_WRITE(V0)
+{
+    Serial.println("Button Tapped");
+    if (param.asInt() == 1)
+    {
+        Blynk.logEvent("push_notifications", "Hey, Blynk push here!");
+    }
+}
